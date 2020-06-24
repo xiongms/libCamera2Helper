93b572d6b46e1b9cbd51c603c4a5d2980fd0bc1a
xiongms <62871434@qq.com>
2020-06-24 10:13:31 +0800

    停止录像时关闭摄像头，再重新打开

--- libCamera2Helper/src/main/java/com/xiongms/libcamera2helper/CameraHelper.kt
+++ libCamera2Helper/src/main/java/com/xiongms/libcamera2helper/CameraHelper.kt
@@ -147,6 +147,12 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
      */
     private var isPreviewing = false
 
+    /**
+     * surface的宽高
+     */
+    private var surfaceWidth: Int = 0
+    private var surfaceHeight: Int = 0
+
 
     init {
         initParameter()
@@ -188,7 +194,9 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
             width: Int,
             height: Int
         ) {
-            openCamera(width, height)
+            surfaceWidth = width
+            surfaceHeight = height
+            openCamera()
         }
 
         /**
@@ -199,6 +207,8 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
             width: Int,
             height: Int
         ) {
+            surfaceWidth = width
+            surfaceHeight = height
             configureTransform(width, height)
         }
 
@@ -451,6 +461,10 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
             ex.printStackTrace()
         }
 
+        if (videoTempFile.exists()) {
+            videoTempFile.delete()
+        }
+
         mMediaRecorder!!.setAudioSource(MediaRecorder.AudioSource.MIC)
         mMediaRecorder!!.setVideoSource(MediaRecorder.VideoSource.SURFACE)
         mMediaRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
@@ -606,9 +620,9 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
      * 打开摄像头
      */
     @SuppressLint("MissingPermission")
-    private fun openCamera(width: Int, height: Int) {
+    private fun openCamera() {
         try {
-            configureTransform(width, height)
+            configureTransform(surfaceWidth, surfaceHeight)
             mCameraManager.openCamera(cameraId!!, mStateCallback, null)
         } catch (e: CameraAccessException) {
             Log.e(context.javaClass.simpleName, "Cannot access the camera.")
@@ -634,13 +648,6 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
             if (mImageReader != null) {
                 mImageReader!!.close()
                 mImageReader = null
-            }
-
-            // 删除临时文件
-            if (videoTempFile.exists()) {
-                if (!videoTempFile.delete()) {
-                    videoTempFile.deleteOnExit()
-                }
             }
 
             if (null != mMediaRecorder) {
@@ -728,7 +735,7 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
     private fun startPreview() {
         startBackgroundThread()
         if (textureView.isAvailable) {
-            openCamera(textureView.width, textureView.height)
+            openCamera()
         } else {
             textureView.surfaceTextureListener = mSurfaceTextureListener
         }
@@ -771,8 +778,7 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
                 if (now - startRecordTime < 1000) {
                     Thread.sleep(1000)
                 }
-                mMediaRecorder?.stop()
-                mMediaRecorder?.reset()
+                stopPreview()
             } catch (ex: Exception) {
                 ex.printStackTrace()
                 onRecordListener?.onError(ex)
@@ -790,7 +796,9 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
                     val fileLock = fos.channel.lock()
                     fileLock?.release()
                     fos.close()
-                    if (videoTempFile.renameTo(destFile)) {
+                    videoTempFile.copyTo(destFile, true, 1000)
+                    if (destFile.exists()) {
+                        destFile.deleteOnExit()
                         onRecordListener?.onSavedRecord(destFile)
                         result = true
                     }
@@ -803,7 +811,7 @@ class CameraHelper(lifecycleOwner: LifecycleOwner, val textureView: AutoFitTextu
             }
 
             // 重新配置参数，进入预览状态
-            readyToPreview()
+            startPreview()
         }
         return result
     }
